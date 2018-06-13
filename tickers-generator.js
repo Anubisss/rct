31619672aed23d589088adc155fefe501de1fb9d
anuka <david.vas1@gmail.com>
2018-06-13 18:31:50 +0200

    Revert "Fix simple error messages."
    This reverts commit 25b89ce36a506033b0bb0563b995cc60a6bf8c42.

--- tickers-generator.js
+++ tickers-generator.js
@@ -19,14 +19,6 @@ const FINVIZ_SCREENER_URL = 'https://finviz.com/screener.ashx?v=111&t='
 const HOST_S3_BUCKET_NAME = process.env.HOST_S3_BUCKET_NAME
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 
-class SimpleError extends Error {
-  constructor(message) {
-    super(message)
-
-    this.name = `${ this.constructor.name }: ${ message }`
-  }
-}
-
 class InstrumentError extends Error {
   constructor(message, invalidElement) {
     super(message)
@@ -98,7 +90,7 @@ function getInstrumentsData() {
         reject(err)
       }
       else if (res.statusCode !== 200) {
-        reject(new SimpleError(`got non 200 response, status code: ${ res.statusCode }`))
+        reject(new Error(`got non 200 response, status code: ${ res.statusCode }`))
       } else {
         resolve(body)
       }
@@ -151,7 +143,7 @@ function selectInstruments(instruments) {
 function renderHtml(instruments) {
   winston.info('renderHtml')
   if (!instruments.length) {
-    throw new SimpleError('HTML renderer got 0 instruments')
+    throw new Error('HTML renderer got 0 instruments')
   }
 
   const templateData = {
